b7e338ca1265284e94f255dbc328b3ce4581b8ec
Frank Wei <frank@frankwei.xyz>
2021-03-29 18:19:37 +0800

    fix: missing `styles` folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
+import { light, dark } from "../themes";
 import 'inter-ui/inter.css'
 // import { useState } from "react";
 
 // import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <GeistProvider>
+  return <GeistProvider themes={[ light, dark ]}>
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>;
