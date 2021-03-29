c36aeb6346978da78695b67a7f9cab6939fb404d
Frank Wei <frank@frankwei.xyz>
2021-03-29 18:19:20 +0800

    feat: ready for our custom themes

---
+++ src/themes/index.ts
@@ -0,0 +1,15 @@
+import { Themes } from '@geist-ui/react'
+
+export const light = Themes.createFromLight({
+  type: 'clight',
+  palette: {
+    success: '#000',
+  },
+})
+
+export const dark = Themes.createFromDark({
+  type: 'cdark',
+  palette: {
+    success: '#000',
+  },
+})
