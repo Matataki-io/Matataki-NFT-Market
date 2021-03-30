913ff92b566c2b200d4c0d139f74d80c65fb4ba0
Frank Wei <frank@frankwei.xyz>
2021-03-30 11:53:20 +0800

    chore: icon lib and next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    //   i18n: {
+    //     locales: ['en-US', 'zh-CN', 'ja-JP'],
+    //     defaultLocale: 'en-US',
+    // }
+  }
+}
