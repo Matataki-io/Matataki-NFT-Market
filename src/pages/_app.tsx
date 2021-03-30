64d9b01364d612db5a8b37d9c128e70bbaa061a6
Frank Wei <frank@frankwei.xyz>
2021-03-30 14:43:12 +0800

    style: fix for bad overflow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps /*, AppContext */ } from "next/app";
+import NextHead from "next/head";
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import { light, dark } from "../themes";
 import 'inter-ui/inter.css'
@@ -9,10 +10,16 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <GeistProvider themes={[ light, dark ]}>
+  return <GeistProvider themes={[light, dark]}>
+          <NextHead>
+        <title>Meta NFT Market</title>
+        <link rel="icon" href="/favicon.ico" />
+      </NextHead>
     <CssBaseline />
     <Header />
-    <Component {...pageProps} />
+    <div className="page-content" style={{ margin: "0 12.5% " }}>
+      <Component {...pageProps}  />
+    </div>
     <Footer />
     </GeistProvider>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,46 @@
 import { Button, ButtonDropdown, Divider, Grid, Text, Tooltip } from '@geist-ui/react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <Button shadow type="secondary">阴影</Button>
-      </main>
-
+    // <div className={styles.container}>
+    //   <main className={styles.main}>
+    //     <Button shadow type="secondary">阴影</Button>
+    //   </main>
+    // </div>
+    <div className="home">
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
+      <p>Foobar</p>
     </div>
   )
 }
