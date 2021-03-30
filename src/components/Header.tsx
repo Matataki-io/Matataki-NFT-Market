1122dfa16e143881669dc17323843de1432e6991
Frank Wei <frank@frankwei.xyz>
2021-03-30 11:52:42 +0800

    basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import { Button, Grid, Input, Link,  } from "@geist-ui/react";
+import NextLink from "next/link";
+import { AlignJustify as MenuIcon } from '@geist-ui/react-icons'
+import { CSSProperties } from "react";
+
+const NavBarBtnStyle: CSSProperties = {
+    margin: "4px"
+};
+
+export function Header() {
+    return <div className="header" style={{ padding: '10px 12.5%' }}>
+        <Grid.Container style={{ marginBottom: '10px' }} justify="center">
+            <Grid xs={16} sm={4}>NFT Market</Grid>
+            <Grid xs={0} sm={10} justify="flex-start">
+                <NextLink href="/">
+                    <Link style={NavBarBtnStyle}>NFTS</Link>
+                </NextLink>
+                <NextLink href="/create">
+                    <Link style={NavBarBtnStyle}>CREATE NFT</Link>
+                </NextLink>
+            </Grid>
+            <Grid xs={0} sm={10} justify="flex-end">
+                <Input size="medium" placeholder="Search NFTs" />
+                <Button size="small" type="secondary">Sign In</Button>
+            </Grid>
+            <Grid xs={8} sm={0}>
+                <Button auto size="small"><MenuIcon /></Button>
+            </Grid>
+        </Grid.Container>
+    </div>
+}
