2547c369da12809950bffcb9fdcc49e3414218b2
xiaotiandada <istianlei@qq.com>
2021-04-01 15:15:27 +0800

    feat: NFT Card

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,14 @@
 
 declare module "*.png"
 declare module "*.svg"
+
+
+export interface NFTProps {
+  id: number,
+  type: string
+  img: string
+  avatar_url: string
+  username: string,
+  title: string
+  time: string|number
+}
