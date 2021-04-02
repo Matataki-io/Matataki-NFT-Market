68a54647ccede7dea5ce5ec109e5ca2f270e23ab
xiaotiandada <istianlei@qq.com>
2021-04-03 00:04:28 +0800

    feat: merged

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,17 @@
-import React, { useState, useMemo, Fragment } from "react";
-import { Input, } from "@geist-ui/react";
-import Link from "next/link";
+import React, { useState, useMemo, Fragment } from 'react';
+import { Input } from '@geist-ui/react';
+import Link from 'next/link';
 import styled from 'styled-components';
-import Logo from "../../assets/images/logo.png";
+import Logo from '../../assets/images/logo.png';
 import Button from '../Button/index';
 import { useWallet } from 'use-wallet';
 
 interface HeaderProps {
-  isCreate: Boolean
-  setIsCreate: (value: boolean) => void
+  isCreate: Boolean;
+  setIsCreate: (value: boolean) => void;
 }
 
-const HeaderComponents: React.FC<HeaderProps> = ({
-  isCreate,
-  setIsCreate
-}) => {
-
+const HeaderComponents: React.FC<HeaderProps> = ({ isCreate, setIsCreate }) => {
   const wallet = useWallet();
   const shortedWalletAccount = useMemo(() => {
     if (wallet.status !== 'connected') return 'Not Connected';
@@ -25,26 +21,28 @@ const HeaderComponents: React.FC<HeaderProps> = ({
   return (
     <StyledHeader>
       <StyledHeaderWrapper>
-        {
-          isCreate ?
-            <Fragment>
-              <Button color="gray" onClick={() => setIsCreate(false)}>Back</Button>
-              <div>
-                <a href="https://matataki.io/">
-                  <Button className="hover-underline">Learn</Button>
-                </a>
-                <Button color="gray">@xiaotian</Button>
-              </div>
-            </Fragment> :
-            <Fragment>
-              <StyledHeaderLeft>
-                <Link href="/">
-                  <StyledHeaderLogo>
-                    <img src={Logo} alt="NFT Logo" />
-                    <h1>NFT Market</h1>
-                  </StyledHeaderLogo>
-                </Link>
-                {/* <StyledHeaderNav>
+        {isCreate ? (
+          <Fragment>
+            <Button color='gray' onClick={() => setIsCreate(false)}>
+              Back
+            </Button>
+            <div>
+              <a href='https://matataki.io/'>
+                <Button className='hover-underline'>Learn</Button>
+              </a>
+              <Button color='gray'>{shortedWalletAccount}</Button>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <StyledHeaderLeft>
+              <Link href='/'>
+                <StyledHeaderLogo>
+                  <img src={Logo} alt='NFT Logo' />
+                  <h1>NFT Market</h1>
+                </StyledHeaderLogo>
+              </Link>
+              {/* <StyledHeaderNav>
 									<Link href="/">
 										<a>NFTS</a>
 									</Link>
@@ -52,39 +50,40 @@ const HeaderComponents: React.FC<HeaderProps> = ({
 										<a>CREATE NFT</a>
 									</Link>
 								</StyledHeaderNav> */}
-              </StyledHeaderLeft>
-              <StyledHeaderContainer>
-                <StyledHeaderSearch placeholder="Search NFTs" />
-                <div>
-                  <a href="https://matataki.io/">
-                    <Button className="hover-underline">Learn</Button>
-                  </a>
-                  <Button color="gray">@xiaotian</Button>
-                  {wallet.status === 'connected' ? (
-                    <>
-                      <StyledHeaderUsername>
-                        {shortedWalletAccount}
-                      </StyledHeaderUsername>
-                      <Button color="dark" onClick={() => wallet.reset()}>
-                        Disconnect
-                      </Button>
-                    </>
-                  ) : (
-                    <Button color="dark" onClick={() => wallet.connect('injected')}>
-                      Connect Wallet
+            </StyledHeaderLeft>
+            <StyledHeaderContainer>
+              <StyledHeaderSearch placeholder='Search NFTs' />
+              <div>
+                <a href='https://matataki.io/'>
+                  <Button className='hover-underline'>Learn</Button>
+                </a>
+                {wallet.status === 'connected' ? (
+                  <>
+                    <Button color='gray'>{shortedWalletAccount}</Button>
+                    <Button color='dark' onClick={() => wallet.reset()}>
+                      Disconnect
                     </Button>
-                  )}
-                  <Button color="dark" onClick={() => setIsCreate(true)}>Create</Button>
-                </div>
-              </StyledHeaderContainer>
-            </Fragment>
-        }
+                  </>
+                ) : (
+                  <Button
+                    color='dark'
+                    onClick={() => wallet.connect('injected')}>
+                    Connect Wallet
+                  </Button>
+                )}
+                <Button color='dark' onClick={() => setIsCreate(true)}>
+                  Create
+                </Button>
+              </div>
+            </StyledHeaderContainer>
+          </Fragment>
+        )}
       </StyledHeaderWrapper>
     </StyledHeader>
-  )
-}
+  );
+};
 
-export default HeaderComponents
+export default HeaderComponents;
 
 const StyledHeader = styled.div`
   color: #fff;
@@ -93,18 +92,18 @@ const StyledHeader = styled.div`
   /* left: 0; */
   /* right: 0; */
   /* background: transparent; */
-	/* box-shadow: 0 2px 10px rgb(0 0 0 / 10%); */
-	box-shadow: 0px 1px 0px 0px #DBDBDB;
-  transition: all .3s;
-	position: sticky;
-	top: 0px;
-	left: 0px;
-	right: 0px;
-	z-index: 3;
-	background: rgba(255, 255, 255, 0.95);
-	backdrop-filter: blur(20px);
-	transform: translateZ(0px);
-	will-change: transform;
+  /* box-shadow: 0 2px 10px rgb(0 0 0 / 10%); */
+  box-shadow: 0px 1px 0px 0px #dbdbdb;
+  transition: all 0.3s;
+  position: sticky;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  z-index: 3;
+  background: rgba(255, 255, 255, 0.95);
+  backdrop-filter: blur(20px);
+  transform: translateZ(0px);
+  will-change: transform;
   &.active {
     background-color: #fff;
     color: #542de0;
@@ -118,7 +117,7 @@ const StyledHeaderWrapper = styled.div`
   align-items: center;
   width: 100%;
   /* max-width: 1480px; */
-	padding: 0px 25px;
+  padding: 0px 25px;
   min-height: 80px;
   /* padding: 0 20px; */
   box-sizing: border-box;
@@ -160,43 +159,10 @@ const StyledHeaderNav = styled.nav`
 `;
 
 const StyledHeaderSearch = styled(Input)`
-	width: 320px !important;
-	margin-right: 16px;
-`
+  width: 320px !important;
+  margin-right: 16px;
+`;
 const StyledHeaderContainer = styled.div`
-	display: flex;
-	align-items: center;
-`
-const StyledHeaderButtonLink = styled.a`
-  display: inline-block;
-`;
-const StyledHeaderButton = styled.button`
-  background-color: #333333;
-  font-size: 14px;
-  font-family: PingFangSC-Medium, PingFang SC;
-  font-weight: 500;
-  color: #ffffff;
-  line-height: 20px;
-  border: none;
-  outline: none;
-  padding: 10px 38px;
-  cursor: pointer;
-  transition: all 0.2s;
-  &:hover {
-    background-color: #222;
-  }
-`;
-
-const StyledHeaderUsername = styled.button`
-  color: rgb(0, 0, 0);
-  background: rgb(230, 230, 230);
-  font-size: 14px;
-  font-family: PingFangSC-Medium, PingFang SC;
-  font-weight: 500;
-  line-height: 20px;
-  border: none;
-  outline: none;
-  padding: 10px 8px;
-  cursor: pointer;
-  transition: all 0.2s;
+  display: flex;
+  align-items: center;
 `;
