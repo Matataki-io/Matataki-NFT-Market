6f00887b0ca0c36524026f5296f152cdd43b884e
xiaotiandada <istianlei@qq.com>
2021-04-08 15:05:29 +0800

    feat: modify profile style

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form, Input, Checkbox, Avatar } from 'antd';
+import ButtonCustom from '../Button/index';
+
 const { TextArea } = Input;
 import styled from 'styled-components';
+import styles from './index.module.scss';
 
 import { UserOutlined } from '@ant-design/icons';
 import { useLogin } from '../../hooks/useLogin';
+
+const { Item } = Form;
 
 type RequiredMark = boolean | 'optional';
 
@@ -13,7 +18,7 @@ const usernamePattern = /^(?=[a-z0-9._]{5,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
 
 const Profile: React.FC<any> = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [form] = Form.useForm();
+  const [formProfile] = Form.useForm();
   const [requiredMark, setRequiredMarkType] = useState<RequiredMark>(
     'optional'
   );
@@ -23,7 +28,6 @@ const Profile: React.FC<any> = () => {
   const showModal = () => {
     setIsModalVisible(true);
   };
-
   const handleOk = () => {
     setIsModalVisible(false);
   };
@@ -31,7 +35,6 @@ const Profile: React.FC<any> = () => {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-
   const onFinish = async (values: any) => {
     console.log('Success:', values);
     let { nickname, bio, username } = values;
@@ -44,78 +47,72 @@ const Profile: React.FC<any> = () => {
   };
 
   return (
-    <>
-      <Button type='primary' onClick={showModal}>
-        Open Modal
-      </Button>
-      <Modal
-        title='Complete your profile'
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        footer={null}>
-        <Form
-          form={form}
-          name='basic'
-          layout='vertical'
-          initialValues={{ requiredMarkValue: requiredMark }}
-          width={470}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          requiredMark={requiredMark}>
-          <StyledUser>
-            <Avatar size={40} icon={<UserOutlined />} />
-            <StyledItemUser>
-              <div>0x3484040A...14ecCcA6</div>
-              <div>Change profile image</div>
-            </StyledItemUser>
-          </StyledUser>
-          <Form.Item
-            label='USERNAME'
-            name='username'
-            required
-            rules={[
-              {
-                required: true,
-                message: 'Please input your username.',
-              },
-              {
-                required: true,
-                pattern: usernamePattern,
-                message: `Only numbers, characters a-z '-' and the length is 5-20 are acceptable.`,
-              },
-            ]}>
-            <Input
-              placeholder='Enter your username'
-              maxLength={42}
-              allowClear
-            />
-          </Form.Item>
-          <Form.Item
-            label='NICKNAME'
-            name='nickname'
-            required
-            rules={[
-              { required: true, message: 'Please input your nickname.' },
-            ]}>
-            <Input placeholder='nickname' maxLength={42} allowClear />
-          </Form.Item>
-          <Form.Item label='BIO' name='bio'>
-            <TextArea
-              maxLength={200}
-              showCount
-              allowClear
-              autoSize={{ minRows: 4, maxRows: 6 }}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button type='primary' htmlType='submit'>
-              Save
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </>
+    <Modal
+      title='Complete your profile'
+      visible={isModalVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      footer={null}>
+      <Form
+        form={formProfile}
+        name='formProfile'
+        layout='vertical'
+        initialValues={{ requiredMarkValue: requiredMark }}
+        width={470}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        requiredMark={requiredMark}
+        className={styles.form}>
+        <StyledUser>
+          <Avatar size={40} icon={<UserOutlined />} />
+          <StyledItemUser>
+            <div>0x3484040A...14ecCcA6</div>
+            <div>Change profile image</div>
+          </StyledItemUser>
+        </StyledUser>
+        <StyledItem
+          label='USERNAME'
+          name='username'
+          required
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username.',
+            },
+            {
+              required: true,
+              pattern: usernamePattern,
+              message: `Only numbers, characters a-z '-' and the length is 5-20 are acceptable.`,
+            },
+          ]}>
+          <StyledItemInput
+            placeholder='Enter your username'
+            maxLength={42}
+            allowClear
+          />
+        </StyledItem>
+        <StyledItem
+          label='NICKNAME'
+          name='nickname'
+          required
+          rules={[{ required: true, message: 'Please input your nickname.' }]}>
+          <StyledItemInput placeholder='nickname' maxLength={42} allowClear />
+        </StyledItem>
+        <StyledItem label='BIO' name='bio'>
+          <StyledItemTextArea
+            maxLength={200}
+            showCount
+            allowClear
+            autoSize={{ minRows: 4, maxRows: 6 }}
+          />
+        </StyledItem>
+        <StyledItem style={{ textAlign: 'right', marginTop: '20px' }}>
+          <ButtonCustom color='dark' type='submit'>
+            Save
+          </ButtonCustom>
+        </StyledItem>
+      </Form>
+    </Modal>
   );
 };
 const StyledUser = styled.div`
@@ -142,5 +139,14 @@ const StyledItemUser = styled.div`
     }
   }
 `;
-
+const StyledItem = styled(Item)`
+  .ant-form-item-label label {
+    /* color: rgba(0, 0, 0, 0.5); */
+    /* font-size: 12px; */
+  }
+`;
+const StyledItemInput = styled(Input)`
+  min-height: 50px;
+`;
+const StyledItemTextArea = styled(TextArea)``;
 export default Profile;
