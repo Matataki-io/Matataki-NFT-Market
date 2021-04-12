ae695788f1e3930a74aebc4ed85883d856535929
Frank Wei <frank@frankwei.xyz>
2021-04-12 17:32:49 +0800

    fix: support when address is null

--- src/hooks/useERC20.ts
+++ src/hooks/useERC20.ts
@@ -1,19 +1,20 @@
 import { ethers } from 'ethers';
 import { useMemo } from 'react';
 import { BaseErc20Factory } from '../blockchain/contracts/BaseErc20Factory';
+import { ZERO_ADDRESS } from '../constant';
 import { currentProvider } from '../constant/providers';
 import { useSigner } from './useSigner';
 
 export function useERC20(tokenAddress: string) {
   const { signer, isSignerReady } = useSigner();
   const token = useMemo(() => {
+    const readonlyProvider = currentProvider as ethers.providers.Provider;
+    if (!tokenAddress)
+      return BaseErc20Factory.connect(ZERO_ADDRESS, readonlyProvider);
     if (isSignerReady(signer)) {
       return BaseErc20Factory.connect(tokenAddress, signer);
     } else {
-      return BaseErc20Factory.connect(
-        tokenAddress,
-        currentProvider as ethers.providers.Provider
-      );
+      return BaseErc20Factory.connect(tokenAddress, readonlyProvider);
     }
   }, [tokenAddress, signer]);
 
