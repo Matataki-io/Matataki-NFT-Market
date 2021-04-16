1306095a151f5eed30103668ed559da15a18ad5b
Frank Wei <frank@frankwei.xyz>
2021-04-16 15:44:23 +0800

    feat: check is mint event

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,4 +1,14 @@
+import { ZERO_ADDRESS } from '../constant';
 import { currentContracts } from '../constant/contracts';
+import { Ask } from '../types/Ask';
+import { MediaLog } from '../types/MediaLog';
 
 export const getTokenOnScan = (tokenId: number | string) =>
   `${process.env.NEXT_PUBLIC_SCAN_PREFIX}/token/${currentContracts?.MEDIA}?a=${tokenId}`;
+
+export function isMediaTokenLog(event: Ask | MediaLog): event is MediaLog {
+  return event.type.slice(3) !== 'Ask';
+}
+export function isMintEvent(event: Ask | MediaLog): event is MediaLog {
+  return isMediaTokenLog(event) && event.from === ZERO_ADDRESS;
+}
