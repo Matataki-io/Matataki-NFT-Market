1e1d93ccd609eaf49eb7e7c199ca1cb17b6dcb29
Frank Wei <frank@frankwei.xyz>
2021-04-16 17:45:36 +0800

    fix: `obj.type` may be undefined

--- src/utils/TypeGuards.ts
+++ src/utils/TypeGuards.ts
@@ -1,5 +1,7 @@
 import { Ask } from '../types/Ask';
 
 export function isBackendAsk(obj: any): obj is Ask {
-  return Boolean(obj.currency) && obj.type.slice(0, 3) === 'Ask';
+  return (
+    Boolean(obj.currency) && Boolean(obj.type) && obj.type.slice(0, 3) === 'Ask'
+  );
 }
