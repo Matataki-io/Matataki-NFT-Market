dc57fc16136fdd4ea7ba0ff762f707bbabdc8177
xiaotiandada <istianlei@qq.com>
2021-04-27 20:02:43 +0800

    feat: home modify nft style

--- src/components/NFTSimple/index.tsx
+++ src/components/NFTSimple/index.tsx
@@ -133,10 +133,10 @@ const StyledNFTContent = styled.div`
       display: block;
       max-width: 100%;
       max-height: 100%;
-      width: auto;
-      height: auto;
+      width: 100%;
+      height: 100%;
       margin: 0px auto;
-      object-fit: contain;
+      object-fit: cover;
     }
   }
   .media-audio {
