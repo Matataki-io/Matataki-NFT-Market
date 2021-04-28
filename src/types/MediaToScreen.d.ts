a06133116ad828af85d4d083ac5504956084c463
Frank Wei <frank@frankwei.xyz>
2021-04-28 17:26:05 +0800

    feat: new fields on user

---
+++ src/types/MediaToScreen.d.ts
@@ -0,0 +1,13 @@
+import type { User } from './User.types';
+import type { MintAndTransferParameters } from './MintAndTransfer';
+
+export interface MediaToScreen {
+  id: number;
+  creator: User;
+  publisher: User;
+  title: string;
+  description: string;
+  tokenURI: string;
+  nonce: number;
+  permitData: MintAndTransferParameters;
+}
