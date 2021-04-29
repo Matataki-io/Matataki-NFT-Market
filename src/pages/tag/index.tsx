da1032061030247b3b68d4a5aa9099931a0af06f
billow <billow.fun@gmail.com>
2021-04-30 07:06:35 +0800

    fear: use antd list

--- src/pages/tag/index.tsx
+++ src/pages/tag/index.tsx
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { getTags } from '../../backend/tag';
 import { Tag } from '../../types/tag';
+import { List } from 'antd';
 
 const TagPage: React.FC = () => {
   const [tags, updateTags] = useState<Tag[]>([]);
   useEffect(() => {
-    console.log('tags');
-    console.log(tags);
     getTags().then(({ data }) => {
-      console.log(data);
       updateTags(data);
     });
   }, []);
   return (
     <>
       {tags && (
-        <div>
-          {tags.map(t => (
-            <div key={t.id}>{t.name}</div>
-          ))}
-        </div>
+        <List
+          bordered
+          dataSource={tags}
+          renderItem={item => <List.Item>{item.name}</List.Item>}
+        />
       )}
     </>
   );
