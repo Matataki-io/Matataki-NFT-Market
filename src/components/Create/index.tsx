e291fd5f9df0306158f5cf01fd409cb1ffcfe895
Frank Wei <frank@frankwei.xyz>
2021-04-30 11:51:45 +0800

    feat: good to upload to backend

--- src/components/Create/index.tsx
+++ src/components/Create/index.tsx
@@ -3,7 +3,11 @@ import styled, { css } from 'styled-components';
 import { useWallet } from 'use-wallet';
 import { useSigner } from '../../hooks/useSigner';
 import { Signer } from 'ethers';
-import { PostMedia } from '../../backend/media';
+import {
+  getNonceByPublisherId,
+  PostMedia,
+  sendToPublisherForPreview,
+} from '../../backend/media';
 import { getGalleryUsers } from '../../backend/user';
 import ButtonCustom from '../Button';
 import NFT from '../NFTCreate';
@@ -83,12 +87,12 @@ const CreateComponents: React.FC<Props> = ({ setIsCreate }) => {
     description: string;
   }>({ name: '', description: '' }); // 备份 formNameAndDescription 数据
   // const [uploadLoading, setUploadLoading] = useState<boolean>(false);
-  const [galleryList, setGalleryList] = useState<Array<any>>([]);
+  const [galleryList, setGalleryList] = useState<User[]>([]);
   const mediaContract = useMedia();
 
   useEffect(() => {
     const fetch = async () => {
-      const data: Array<User> = await getGalleryUsers();
+      const data = await getGalleryUsers();
       setGalleryList(data);
     };
     fetch();
@@ -403,12 +407,17 @@ const CreateComponents: React.FC<Props> = ({ setIsCreate }) => {
       gallery
     );
 
+    const galleryUser = galleryList.find(u => u.address === gallery);
+    if (!galleryUser) throw new Error('Unable to find the gallery user');
+    const nonce = await getNonceByPublisherId(galleryUser.id);
+
     const res = await GenerateCreationSignature(
       tokenURI,
       metadataURI,
       contentHash,
       metadataHash,
       gallery,
+      nonce,
       Number(creatorShare),
       signer
     );
@@ -417,17 +426,24 @@ const CreateComponents: React.FC<Props> = ({ setIsCreate }) => {
       message.error('not res');
       return;
     }
-    const resp = await mediaContract.mintAndTransferWithSig(
-      wallet.account as string,
-      res?.data,
-      res?.bidShares,
-      res?.to,
-      res?.sig
-    );
-    const receipt = await resp.wait();
-    console.info('re', receipt);
-    alert('txHash' + receipt.transactionHash);
-
+    await sendToPublisherForPreview(galleryUser.id, {
+      nonce,
+      title: nameAndDescription.name,
+      description: nameAndDescription.description,
+      tokenURI,
+      permitData: res,
+    });
+    // const resp = await mediaContract.mintAndTransferWithSig(
+    //   wallet.account as string,
+    //   res?.data,
+    //   res?.bidShares,
+    //   res?.to,
+    //   res?.sig
+    // );
+    // const receipt = await resp.wait();
+    // console.info('re', receipt);
+    // alert('txHash' + receipt.transactionHash);
+    message.success('上传成功，等待画廊审核');
     setIsCreate(false);
   };
 
@@ -654,7 +670,7 @@ const CreateComponents: React.FC<Props> = ({ setIsCreate }) => {
               </Form.Item>
               <Form.Item label='Gallery' name='gallery'>
                 <Select placeholder='Select a gallery'>
-                  {galleryList.map((i: User, idx: number) => (
+                  {galleryList.map((i, idx: number) => (
                     <Option key={`${idx}-${i.address}`} value={i.address}>
                       <span>
                         <Avatar
