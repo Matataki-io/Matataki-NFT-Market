058e593cb82a1025f6aebe973bb9df195ba92ca3
Frank Wei <frank@frankwei.xyz>
2021-05-10 14:28:53 +0800

    feat: support for WETH-like

--- src/constant/contracts.ts
+++ src/constant/contracts.ts
@@ -34,6 +34,19 @@ export const TOKENS: {
   },
 };
 
+export const WETH: {
+  [chainId in ChainId]: string;
+} = {
+  [ChainId.MAINNET]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  [ChainId.ROPSTEN]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  [ChainId.RINKEBY]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+  [ChainId.GÖRLI]: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+  [ChainId.KOVAN]: '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
+  [ChainId.BSC_MAINNET]: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+  [ChainId.BSC_TESTNET]: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+};
+
 export const currentContracts = CONTRACTS[currentChainId];
 export const currentSupportedTokens = TOKENS[currentChainId];
 export const currentMulticallAddress = MULTICALL_NETWORKS[currentChainId];
+export const currentWETH = WETH[currentChainId];
