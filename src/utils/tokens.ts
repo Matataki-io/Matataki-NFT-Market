6f3c860b98697dca68ce8842d1c91bcee6e86bf7
Frank Wei <frank@frankwei.xyz>
2021-05-13 18:29:09 +0800

    fix: for test

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -2,6 +2,11 @@ import { utils } from 'ethers';
 import { ChainId } from '../constant';
 import { currentSupportedTokens, TOKENS } from '../constant/contracts';
 
+/**
+ * 静态获取 Token 小数点数据
+ * @param address token地址
+ * @returns
+ */
 export const getDecimalOf = (address: string) => {
   if (!address) return 18;
   const checksumedAddress = utils.getAddress(address);
@@ -12,7 +17,12 @@ export const getDecimalOf = (address: string) => {
       return 6;
     case TOKENS[ChainId.MAINNET]?.WBTC:
       return 8;
-
+    case TOKENS[ChainId.RINKEBY]?.USDT:
+      return 6;
+    case TOKENS[ChainId.RINKEBY]?.USDC:
+      return 6;
+    case TOKENS[ChainId.RINKEBY]?.WBTC:
+      return 8;
     //     break;
 
     default:
