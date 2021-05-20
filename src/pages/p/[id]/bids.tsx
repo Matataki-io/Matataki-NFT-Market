567718694545f1eb2e64c0e8b73189a31da2c61b
xiaotiandada <istianlei@qq.com>
2021-05-20 13:13:27 +0800

    feat: modify bids page style

--- src/pages/p/[id]/bids.tsx
+++ src/pages/p/[id]/bids.tsx
@@ -1,13 +1,13 @@
-import { Button, Table, Text } from '@geist-ui/react';
+import { Table, Text } from '@geist-ui/react';
 import { utils } from 'ethers';
 import { useRouter } from 'next/router';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 import { useWallet } from 'use-wallet';
 import styled from 'styled-components';
-import { Spin } from 'antd';
+import { Spin, Button, Typography, message, notification } from 'antd';
 
 import { backendSWRFetcher } from '../../../backend/media';
 import { useMedia } from '../../../hooks/useMedia';
@@ -18,6 +18,9 @@ import { Bid } from '../../../types/ContractTypes';
 import { getBidFor } from '../../../utils/MarketHelper';
 import { Decimal } from '../../../utils/Decimal';
 import Link from 'next/link';
+import { shortedWalletAccount } from '../../../utils/index';
+
+const { Paragraph } = Typography;
 
 function notNull(b: Bid | null): b is Bid {
   return Boolean(b);
@@ -32,6 +35,10 @@ export default function Bids() {
     backendSWRFetcher
   );
   const [activeBids, setActiveBids] = useState<Record<string, Bid | null>>({});
+
+  const triggerReloadBids = useCallback(() => mutate(`/media/${id}/bids`), [
+    id,
+  ]);
 
   const getBidsDetail = useCallback(
     async (bidderList: string[]) => {
@@ -49,7 +56,7 @@ export default function Bids() {
     );
 
     getBidsDetail(bidderList);
-  }, [id, data]);
+  }, [id, data, getBidsDetail]);
 
   const media = useMedia();
   const { isMeTheOwner } = useMediaToken(id as string);
@@ -61,21 +68,75 @@ export default function Bids() {
         sellOnShare: bid.sellOnShare,
       });
       const receipt = await tx.wait();
-      alert(`Please check on EtherScan, txHash: ${receipt.transactionHash}`);
+      message.info(
+        `Please check on EtherScan, txHash: ${receipt.transactionHash}`
+      );
     },
     [id, media]
   );
 
+  const openNotification = ({
+    description,
+    duration = 4.5,
+    key = '',
+  }: {
+    description: string;
+    duration?: number | null;
+    key?: string;
+  }) => {
+    notification.open({
+      message: 'Notification Title',
+      description: description,
+      duration: duration,
+      key: key,
+    });
+  };
+
   const removeBid = useCallback(async () => {
-    if (!id) return;
-    const tx = await media.removeBid(id as string);
-    const receipt = await tx.wait();
-    alert(`Please check on EtherScan, txHash: ${receipt.transactionHash}`);
-  }, [id, media]);
+    if (!id) {
+      message.warning('not id');
+      return;
+    }
+    try {
+      const tx = await media.removeBid(id as string);
+
+      const keyOne = `open${Date.now()}`;
+      openNotification({
+        description:
+          'Deleting bid... Wait for the contract confirmation, please do not refresh or leave the page.',
+        duration: null,
+        key: keyOne,
+      });
+
+      const receipt = await tx.wait();
+
+      await triggerReloadBids();
+
+      notification.close(keyOne);
+      openNotification({
+        description: `Please check on EtherScan, txHash:${
+          receipt.transactionHash ? 'txHash:' + receipt.transactionHash : ''
+        }`,
+      });
+    } catch (e) {
+      console.log(e.toString());
+    }
+  }, [id, media, triggerReloadBids]);
 
   const activeBidsList = useMemo(() => {
     const list = Object.values(activeBids).filter(i => notNull(i)) as Bid[];
     return list.map(log => {
+      const bidder = () => {
+        return (
+          <a
+            href={`${process.env.NEXT_PUBLIC_SCAN_PREFIX}/address/${log.bidder}`}
+            target='_blank'
+            rel='noopener noreferrer'>
+            {shortedWalletAccount(log.bidder || '')}
+          </a>
+        );
+      };
+
       const price = `${utils.formatUnits(
         log.amount,
         getDecimalOf(log.currency)
@@ -88,11 +149,11 @@ export default function Bids() {
             </Button>
           );
         return isMeTheOwner ? (
-          <Button type='error' auto size='mini' onClick={() => acceptBid(log)}>
+          <Button type='primary' size='small' onClick={() => acceptBid(log)}>
             Accept Bid and Transfer
           </Button>
         ) : log.bidder === wallet.account ? (
-          <Button type='secondary' auto size='mini' onClick={() => removeBid()}>
+          <Button size='small' danger onClick={() => removeBid()}>
             Remove Bid
           </Button>
         ) : (
@@ -100,66 +161,109 @@ export default function Bids() {
         );
       };
       const sellOnShare = `${utils.formatUnits(log.sellOnShare.value, 18)}%`;
-      return { ...log, price, acceptBidBtn, sellOnShare };
+      return { ...log, bidder, price, acceptBidBtn, sellOnShare };
     });
-  }, [activeBids, wallet]);
+  }, [activeBids, isMeTheOwner, acceptBid, removeBid, wallet]);
   if (!id) {
-    return <div className='loading'>Loading ID from Router</div>;
+    return (
+      <StyledWrapper>
+        <StyledWrapperLoading>
+          <Spin tip='Loading ID from Route...'></Spin>
+        </StyledWrapperLoading>
+      </StyledWrapper>
+    );
   }
 
   if (data) {
     const renderedData = data?.map(log => {
+      const bidder = () => {
+        return (
+          <a
+            href={`${process.env.NEXT_PUBLIC_SCAN_PREFIX}/address/${log.bidder}`}
+            target='_blank'
+            rel='noopener noreferrer'>
+            {shortedWalletAccount(log.bidder || '')}
+          </a>
+        );
+      };
+
       dayjs.extend(relativeTime);
       const date = dayjs(log.at.timestamp * 1000).fromNow();
       const price = `${utils.formatUnits(
         log.amount,
         getDecimalOf(log.currency)
       )} ${getSymbolOf(log.currency)}`;
-      return { ...log, date, price };
+      return { ...log, bidder, date, price };
     });
 
     return (
       <StyledWrapper>
-        <Text h1>现在的出价记录</Text>
-        <p>Current Account: {wallet.account}</p>
-        <Table data={activeBidsList}>
-          <Table.Column prop='bidder' label='Bidder' />
-          <Table.Column prop='price' label='Price' />
-          <Table.Column prop='sellOnShare' label='Sell On Share' />
-          <Table.Column prop='acceptBidBtn' label='Action' />
-        </Table>
-        <Link href={`/p/${id}/bid`}>
-          <Button type='secondary'>Set Bid on this artwork</Button>
-        </Link>
-        <Text h1>历史出价日志</Text>
-        <Table data={renderedData}>
-          <Table.Column prop='bidder' label='买家' />
-          <Table.Column prop='price' label='价格' />
-          <Table.Column prop='status' label='状态' />
-          <Table.Column prop='date' label='日期' />
-        </Table>
+        <StyledItem>
+          <Text h2>现在的出价记录</Text>
+          <Paragraph>
+            Current Account:{' '}
+            <a
+              href={`${process.env.NEXT_PUBLIC_SCAN_PREFIX}/address/${wallet.account}`}
+              target='_blank'
+              rel='noopener noreferrer'>
+              {shortedWalletAccount(wallet.account || '')}
+            </a>
+          </Paragraph>
+          <Table data={activeBidsList}>
+            <Table.Column prop='bidder' label='Bidder' />
+            <Table.Column prop='price' label='Price' />
+            <Table.Column prop='sellOnShare' label='Sell On Share' />
+            <Table.Column prop='acceptBidBtn' label='Action' />
+          </Table>
+          <StyledBox>
+            <Link href={`/p/${id}/bid`}>
+              <Button>Set Bid on this artwork</Button>
+            </Link>
+          </StyledBox>
+        </StyledItem>
+
+        <StyledItem>
+          <Text h2>历史出价日志</Text>
+          <Table data={renderedData}>
+            <Table.Column prop='bidder' label='买家' />
+            <Table.Column prop='price' label='价格' />
+            <Table.Column prop='status' label='状态' />
+            <Table.Column prop='date' label='日期' />
+          </Table>
+        </StyledItem>
       </StyledWrapper>
     );
   }
   return (
-    <StyledWrapperLoading>
-      <Spin tip='Loading Bids...'></Spin>
-    </StyledWrapperLoading>
+    <StyledWrapper>
+      <StyledWrapperLoading>
+        <Spin tip='Loading Bids...'></Spin>
+      </StyledWrapperLoading>
+    </StyledWrapper>
   );
 }
 
 const StyledWrapper = styled.div`
-  max-width: 900px;
+  flex: 1;
+  width: 900px;
   margin: 0 auto;
   padding: 40px 0 100px;
+  @media screen and (max-width: 900px) {
+    width: 100%;
+  }
 `;
 
 const StyledWrapperLoading = styled.div`
-  max-width: 900px;
-  margin: 0 auto;
-  padding: 40px 0 100px;
-  min-height: 400px;
+  padding: 40px 0 0;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const StyledBox = styled.div`
+  margin: 10px 0;
+`;
+
+const StyledItem = styled.div`
+  margin: 40px 0;
 `;
