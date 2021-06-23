221901aeaafc0363e0c6a22f9a0ea5e725477a02
Frank Wei <frank@frankwei.xyz>
2021-06-23 12:19:13 +0800

    feat: simple demo

---
+++ src/pages/_dev/getProfile.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@geist-ui/react';
+import React, { useCallback, useState, useEffect } from 'react';
+
+import { useWallet } from 'use-wallet';
+import { MatatakiApi } from '../../backend/matatakiApi';
+import { MatatakiUserProfile } from '../../types/MatatakiType';
+
+export default function GetProfilePage() {
+  const wallet = useWallet();
+  const [profile, setProfile] = useState<null | MatatakiUserProfile>(null);
+
+  const getProfile = useCallback(
+    async (account: string) => {
+      const result = await MatatakiApi.getProfileByWallet(account);
+      setProfile(result);
+    },
+    [setProfile]
+  );
+
+  useEffect(() => {
+    if (!wallet.account) return;
+    getProfile(wallet.account);
+  }, [wallet.account]);
+
+  if (wallet.status !== 'connected') {
+    return (
+      <div>
+        <Button onClick={() => wallet.connect('injected')}>
+          Connect with MetaMask
+        </Button>
+      </div>
+    );
+  }
+
+  return <div>{JSON.stringify(profile)}</div>;
+}
